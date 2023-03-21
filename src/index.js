284fc81f022ed4319ae706bbfa03ad44826e52cf
pratokko <pratokko1@gmail.com>
2023-03-21 16:15:32 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const title = "Dog Man: Twenty Thousand Fleas Under the Sea";
+const image = "./images/book-1.jpg";
+const author = " Dav Pilkey";
+const BookList = () => {
+  return (
+    <section className="book-list">
+      <Book image={image} title={title} author={author} />
+      <Book image={image} title={title} author={author} />
+      <Book image={image} title={title} author={author} />
+    </section>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = (props) => {
+  return (
+    <article className="book">
+      <>
+        <img className="book-img" src={props.image} alt={props.title} />
+        <h2 className="book-title">{props.title}</h2>
+        <h4
+          style={{
+            color: "#fff",
+            marginTop: "1rem",
+            backgroundColor: "blueviolet",
+            borderRadius: "5px",
+          }}
+        >
+          {props.author}
+        </h4>
+      </>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
